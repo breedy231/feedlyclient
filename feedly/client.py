90e7baad48d582fb6c04ecd0de3040f27ff72abb
breedy231 <breedy231@gmail.com>
2022-10-06 17:56:05 -0500

    improved yt scraping bits

--- feedly/client.py
+++ feedly/client.py
@@ -1,6 +1,8 @@
+from email.policy import default
 import re
 from sys import api_version
 from typing import List
+from unittest import result
 import httpx
 import json
 from tqdm import tqdm
@@ -14,7 +16,9 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-from articles import Article
+from articles import Article, article
+
+YOUTUBE_URL = 'https://www.youtube.com/watch?v='
 
 YOUTUBE_URL = 'https://www.youtube.com/watch?v='
 
@@ -43,6 +47,9 @@ class FeedlyApiClient:
     API_SERVICE_NAME = "youtube"
     API_VERSION = "v3"
     SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+    VIDEOS_TO_URLS = defaultdict(set)
+    TITLES_TO_DUPLICATES = defaultdict(set)
 
     def __init__(self, client_id, api_key, secrets_file_loc):
         self.client_id = client_id
@@ -125,49 +132,89 @@ class FeedlyApiClient:
 
     def get_all_youtube_links(self):
         first_regex = re.compile('^.*([a-zA-z0-9]{11}).*$')
+        first_again_regex = re.compile('^.*([a-zA-Z0-9]{8}-[a-zA-Z0-9}]{2}).*$')
         second_regex = re.compile('.*([a-zA-z0-9]{4}-[a-zA-z0-9]{6}).*')
         third_regex = re.compile('.*\/([a-zA-z0-9]{11})\?.*')
         verge_regex = re.compile('.*\/([a-zA-z0-9]{11}).*')
         colossal_regex = re.compile('.*\/([a-zA-z0-9]{11})\/.*')
-        regexes = [first_regex, second_regex, third_regex]
+        regexes = [first_regex, first_again_regex, second_regex, third_regex]
         all_youtube_links = set()
 
         unread_articles = self.get_all_unread_articles()
-        all_links = [item['alternate'][0]['href'] for item in unread_articles]
+
+        all_links = []
+        # TODO fix CPM querying (either in Feedly or with request)
+        for article in unread_articles:      
+            try:
+                main_article_link = article['alternate'][0]['href']
+                self.TITLES_TO_DUPLICATES['title'].update(main_article_link)
+                all_links.append(main_article_link)
+            except KeyError:
+                continue
 
         for link in tqdm(all_links):
             external_links = []
-            response = httpx.get(link)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            external_links = external_links + [link.get('href') for link in soup.find_all('a')]
-            
-            if len(external_links) > 0:
-                result_links = [video for video in external_links if video is not None and self._link_has_video(video)]
-                all_youtube_links.update(result_links)
-            
-            other_links = [hyper.get('data-src') for hyper in soup.find_all('iframe')]   
-            if len(other_links) > 0:
-                videos = self._get_avclub_youtube_ids(other_links, regexes)
-                all_youtube_links.update(videos)
+            result_videos = []
 
-            other_other_links = [hyper.get('src') for hyper in soup.find_all('iframe')]
-            if len(other_other_links) > 0:
-                videos = self._get_avclub_youtube_ids(other_other_links, regexes)
-                all_youtube_links.update(videos)
+            try:
+                response = httpx.get(link)
+            except httpx.ReadTimeout:
+                print(f'Read timeout for link: {link}')
+
+            soup = BeautifulSoup(response.content, 'html.parser')
 
             if 'verge' in link:
-                videos = self._get_verge_youtube_ids(soup, verge_regex)
-                all_youtube_links.update(videos)
+                result_videos = self._get_verge_youtube_ids(soup, verge_regex)
             
-            if 'colossal' in link:
-                videos = self._get_colossal_youtube_ids(soup, colossal_regex)
+            # TODO fix
+            # if 'colossal' in link:
+            #     result_videos = self._get_colossal_youtube_ids(soup, colossal_regex)
+
+            external_links = external_links + [link.get('href') for link in soup.find_all('a')]
+            other_links = [hyper.get('data-src') for hyper in soup.find_all('iframe')]
+            other_other_links = [hyper.get('src') for hyper in soup.find_all('iframe')]
+
+            if len(external_links) > 0:
+                first_links_videos = [video for video in external_links if video is not None and self._link_has_video(video)]
+            else: 
+                first_links_videos = []
+            
+            if len(other_links) > 0:
+                second_links_videos = self._get_avclub_youtube_ids(other_links, regexes)
+            else: 
+                second_links_videos = []
+            
+            if len(other_other_links) > 0:
+                third_links_videos = self._get_avclub_youtube_ids(other_other_links, regexes)
+            else:
+                third_links_videos = []
+            
+            result_videos.extend(item for item in first_links_videos)
+            result_videos.extend(item for item in second_links_videos)
+            result_videos.extend(item for item in third_links_videos)
+
+            bad_tag_manager_url = 'v=etagmanager'
+            
+            has_bad_yt_video = len([url for url in result_videos if 'v=ideo' in url]) > 0
+
+            if has_bad_yt_video:
+                raise Exception(f'Bad YT Video links found for link {link}. {result_videos}')
+            else:
+                all_youtube_links.update(result_videos)
+                self._add_new_videos_to_map(result_videos, link)
         
         return list(all_youtube_links)
 
     def _get_colossal_youtube_ids(self, soup:BeautifulSoup, regex: re.Pattern):
         videos = []
         divs = soup.find_all('div', _class='ytp-cued-thumbnail-overlay-image')
-        style_string = divs[0]['style']
+        if divs:
+            try:
+                style_string = divs[0]['style']
+            except IndexError as e:
+                return []
+        else:
+            return []
         is_match = regex.match(style_string)
         if is_match:
             watch_link = f"{YOUTUBE_URL}{is_match.groups()[0]}"
@@ -177,30 +224,41 @@ class FeedlyApiClient:
     def _get_verge_youtube_ids(self, soup:BeautifulSoup, regex: re.Pattern):
         data_script = soup.find_all('script')[-1]
         loaded_script = json.loads(data_script.text)
-        page_components = loaded_script['props']['pageProps']['hydration']['responses'][0]['data']['entity']['body']['components']
-        all_videos = []
-        for page_component in page_components:
-            if page_component['__typename'] == 'EntryBodyEmbed':
-                embed_html = page_component['embed']['embedHtml'] 
-                is_match = regex.match(embed_html)
-                if is_match:
-                    watch_link = f"{YOUTUBE_URL}{is_match.groups()[0]}"
-                    all_videos.append(watch_link)
-        return all_videos
+        # TODO fix key access here for sale articles
+        try:
+            page_components = loaded_script['props']['pageProps']['hydration']['responses'][0]['data']['entity']['body']['components']
+            all_videos = []
+            for page_component in page_components:
+                if page_component['__typename'] == 'EntryBodyEmbed':
+                    embed_html = page_component['embed']['embedHtml'] 
+                    is_match = regex.match(embed_html)
+                    if is_match:
+                        watch_link = f"{YOUTUBE_URL}{is_match.groups()[0]}"
+                        all_videos.append(watch_link)
+            return all_videos
+        except KeyError:
+            return []
     
     def _get_avclub_youtube_ids(self, links: List[str], strings_to_match: List[re.Pattern]) -> List[str]:
         videos = []
-        filtered_links = [item for item in links if item is not None]
+        filtered_links = [item for item in links if item is not None and 'google' not in item]
         for link in filtered_links:
             for string in strings_to_match:
                 is_match = string.match(link)
                 if is_match:
                     watch_link = f"{YOUTUBE_URL}{is_match.groups()[0]}"
                     videos.append(watch_link)
+                    break
         return videos
     
     def _link_has_video(self, url: str) -> bool:
         return 'www.youtube.com/watch?v=' in url or 'vimeo' in url
+    
+    # TODO fix
+    def _add_new_videos_to_map(self, video_list: List[str], article_link: str):
+        for video in video_list:
+            self.VIDEOS_TO_URLS[video].update(article_link)
+        return True
 
     def add_video_to_playlist(self, video_id: str):
          # Get credentials and create an API client
